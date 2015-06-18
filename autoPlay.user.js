cd1c631614b5b6cb41dcfc77398f7f21d27daa77
shadghost <rahartshorn@alaska.edu>
2015-06-17 23:28:17 -0700

    updated with blole's idea

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -1293,7 +1293,7 @@
 
 	function fireWormhole(itemId) {
 		//Wait 1/2 second and fire
-		return setInterval(function(){s().g_Minigame.m_CurrentScene.TryAbility(document.getElementById('abilityitem_' + 26).childElements()[0]); }, 100);
+		return setInterval(function(){s().setInterval(function(){g_Minigame.m_CurrentScene.m_rgAbilityQueue.push({'ability': 26})}, 100); }, 100);
 	}
 
 	function useWormholeIfRelevant() {
