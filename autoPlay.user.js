535d0c9ced42cd8314c078e549b6be077c5e918a
codeRitter <codeRitter@users.noreply.github.com>
2015-06-18 12:02:38 -0400

    Reverting Throw money
    Silly me, and my copy pasting from Tampermonkey. Reverting accidental addition of Throw Money.

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -1361,8 +1361,6 @@
             wormholeTimeout(800);
             wormholeTimeout(900);
 			advLog('Less than ' + control.minsLeft + ' minutes for game to end. Triggering wormholes...', 2);
-		} else if (isNearEndGame() && tryUsingItem(ABILITIES.THROW_MONEY_AT_SCREEN)) {
-			advLog('Less than ' + control.minsLeft + ' minutes for game to end. Throwing money at screen for no particular reason...', 2);
 		}
 	}
     
